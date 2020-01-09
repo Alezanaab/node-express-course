8a08b18955567e19cb65c1dffa9bfc84cd94e0b1
AlezanaAbrego <aabrego@unitedvirtualities.com>
2020-01-09 09:28:52 -0700

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.listen(8000, function () {
-    console.log("server is running")
+app.get('/users', function (req, res) {
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    })
 })
+
+
+app.listen(8000, function () { console.log('server is listening') })
