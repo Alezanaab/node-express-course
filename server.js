a341a2942d073fa59cc24e344ce0acdbff40034f
AlezanaAbrego <aabrego@unitedvirtualities.com>
2020-01-09 09:31:14 -0700

    add mockUserData

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const mockUserData=[
+	{name:'Mark'},
+	{name:'Jill'}
+]
+
 app.get('/users', function (req, res) {
     res.json({
         success: true,
